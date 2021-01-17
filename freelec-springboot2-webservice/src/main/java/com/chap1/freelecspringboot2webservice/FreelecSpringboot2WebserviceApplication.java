04ac4a76f66455a7a98cafd22738b40ca6ade714
amaruak00 <amaruak00@hanwha.com>
2021-01-17 21:51:49 +0900

    third commit

--- freelec-springboot2-webservice/src/main/java/com/chap1/freelecspringboot2webservice/FreelecSpringboot2WebserviceApplication.java
+++ freelec-springboot2-webservice/src/main/java/com/chap1/freelecspringboot2webservice/FreelecSpringboot2WebserviceApplication.java
@@ -1,13 +1,13 @@
-package com.chap1.freelecspringboot2webservice;
+// package com.chap1.freelecspringboot2webservice;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+// import org.springframework.boot.SpringApplication;
+// import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class FreelecSpringboot2WebserviceApplication {
+// @SpringBootApplication
+// public class FreelecSpringboot2WebserviceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FreelecSpringboot2WebserviceApplication.class, args);
-	}
+// 	public static void main(String[] args) {
+// 		SpringApplication.run(FreelecSpringboot2WebserviceApplication.class, args);
+// 	}
 
-}
+// }
--- freelec-springboot2-webservice/src/test/java/com/chap1/freelecspringboot2webservice/FreelecSpringboot2WebserviceApplicationTests.java
+++ freelec-springboot2-webservice/src/test/java/com/chap1/freelecspringboot2webservice/FreelecSpringboot2WebserviceApplicationTests.java
@@ -1,13 +1,13 @@
-package com.chap1.freelecspringboot2webservice;
+// package com.chap1.freelecspringboot2webservice;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+// import org.junit.jupiter.api.Test;
+// import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class FreelecSpringboot2WebserviceApplicationTests {
+// @SpringBootTest
+// class FreelecSpringboot2WebserviceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+// 	@Test
+// 	void contextLoads() {
+// 	}
 
-}
+// }
