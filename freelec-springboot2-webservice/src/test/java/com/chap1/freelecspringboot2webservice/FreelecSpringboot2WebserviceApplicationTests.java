e9c2ca86ea20fde955d83399984942775eaf7cba
amaruak00 <amaruak00@hanwha.com>
2021-01-17 13:39:26 +0900

    first commit

---
+++ freelec-springboot2-webservice/src/test/java/com/chap1/freelecspringboot2webservice/FreelecSpringboot2WebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chap1.freelecspringboot2webservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreelecSpringboot2WebserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
